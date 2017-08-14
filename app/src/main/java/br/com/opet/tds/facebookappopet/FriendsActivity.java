f5f48c0d0e266ac730bfb286d8d34250a28881b9
mlmausbach <mlmausbach@gmail.com>
2017-08-14 09:37:32 -0300

    alteração

--- app/src/main/java/br/com/opet/tds/facebookappopet/FriendsActivity.java
+++ app/src/main/java/br/com/opet/tds/facebookappopet/FriendsActivity.java
@@ -2,29 +2,51 @@ package br.com.opet.tds.facebookappopet;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.HttpMethod;
+import com.facebook.login.widget.ProfilePictureView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class FriendsActivity extends Activity {
 
     private String facebookID;
+    ProfilePictureView profile;
+    TextView textNome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+        profile = (ProfilePictureView) findViewById(R.id.profile);
+        textNome = (TextView) findViewById(R.id.textNome);
 
         facebookID = getIntent().getStringExtra("FB_ID");
-        loadFriendList();
+
+        AccessToken token = AccessToken.getCurrentAccessToken();
+
+        profile.setProfileId(token.getUserId());
+
+        loadUserName();
     }
 
-    public void loadFriendList(){
-       new GraphRequest(AccessToken.getCurrentAccessToken(), facebookID + "/friends", null, HttpMethod.GET, new GraphRequest.Callback() {
+    public void loadUserName(){
+       new GraphRequest(AccessToken.getCurrentAccessToken(), "me", null,
+               HttpMethod.GET, new GraphRequest.Callback() {
             @Override
             public void onCompleted(GraphResponse response) {
+                JSONObject obj = response.getJSONObject();
+                try {
+                    String nome = obj.getString("name");
+                    textNome.setText("Olá!" + nome);
+                }  catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
             }
         }).executeAsync();
